fa6dd675736d9edd9505e1d9ce3cabef1ec5b9e4
John Dodd <harbingerofvengeance@gmail.com>
2023-03-12 19:03:32 -0700

    Started on App, saving changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id='row' className='flexBox'>
+          <h1>
+            Calorie-Boy
+          </h1>
+        </div>
+        <div id='row' className='flexBox'>
+          <p>
+            Your one stop shop for counting calories
+            quickly and easily without a bulky app
+          </p>
+        </div>
+        <div id='row' className='flexBox'>
+          <input className='addText' type="text" name="food" onChange={() => {console.log('ravioli')}}/>
+          <Button className='addButton btn btn-primary '>Add</Button>
+        </div>
+        <div id='row' className='flexBox'>
+          <ul>
+            <li>Wow, what a list item :)</li>
+            <li>Wow, what a list item :)</li>
+            <li>Wow, what a list item :)</li>
+          </ul>
+        </div>
       </header>
     </div>
   );
