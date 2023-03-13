1765fe54b17c3fdc6490603732c88152d8c6315d
John Dodd <harbingerofvengeance@gmail.com>
2023-03-12 18:13:07 -0700

    Updates

---
+++ precache-manifest.3309eb520b2580634b8656a1d19578dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5ee16e5d8e476a09146",
+    "url": "/Calorie-Boy/static/css/main.87fea29c.chunk.css"
+  },
+  {
+    "revision": "d5ee16e5d8e476a09146",
+    "url": "/Calorie-Boy/static/js/main.d5ee16e5.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Calorie-Boy/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/Calorie-Boy/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "09718363d0e10396a3f5",
+    "url": "/Calorie-Boy/static/js/runtime~main.09718363.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Calorie-Boy/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "fbae644348328bea41c5453292d4c6df",
+    "url": "/Calorie-Boy/index.html"
+  }
+];
